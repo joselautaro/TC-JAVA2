f136aeef10bab3dfa74ff3fa17dc1e505224f837
Lautaro Montaña <joselautarom@gmail.com>
2025-03-10 08:01:55 -0300

    Operaciones double

---
+++ modulo1/OperacionesDouble.java
@@ -0,0 +1,10 @@
+package modulo1;
+
+public class OperacionesDouble {
+    public static void main(String[] args) {
+        double x = 8.5;
+        double y = 3.5;
+
+        System.out.println("suma: " + (x + y));
+    }
+}
