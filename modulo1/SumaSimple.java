a5c0eae3c4f318f3c53f167bb05e161a34de7cc4
Lautaro Montaña <joselautarom@gmail.com>
2025-03-10 07:54:50 -0300

    Suma simple

---
+++ modulo1/SumaSimple.java
@@ -0,0 +1,11 @@
+package modulo1;
+
+public class SumaSimple {
+    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println(a + b);
+    }
+
+}
