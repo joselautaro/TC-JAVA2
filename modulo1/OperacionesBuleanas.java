7f3301eb368ad9d53c4c19bbdb8d3a4c57e315a6
Lautaro Montaña <joselautarom@gmail.com>
2025-03-10 07:59:39 -0300

    Operaciones buleanas

---
+++ modulo1/OperacionesBuleanas.java
@@ -0,0 +1,12 @@
+package modulo1;
+
+public class OperacionesBuleanas {
+    public static void main(String[] args) {
+        boolean esMayorDeEdad = true;
+
+        boolean tieneLicenciaDeConducir = false;
+
+        System.out.println("Es mayor de edad: " + esMayorDeEdad);
+        System.out.println("Tienen licencia de conducir: " + tieneLicenciaDeConducir);
+    }
+}
