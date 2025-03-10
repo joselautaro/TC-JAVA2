0b7b753158a5085fd77c5136d2a4c15241e9d05b
Lautaro Montaña <joselautarom@gmail.com>
2025-03-10 06:57:42 -0300

    Inicio de formacion: datos primitivos

---
+++ modulo1/DatosPrimitivos.java
@@ -0,0 +1,44 @@
+package modulo1;
+
+public class DatosPrimitivos {
+
+    public static void main(String[] args) {
+        
+            //Enteros 
+            //descripcion: entero de 8 bits con signo
+            // byte edad = 25;
+
+
+            //short
+            //descripcion: enteros con 16 bits con signo
+            //short salario = 15000;
+
+            //int 
+            //descripcion: entero de 32 bits
+            //int poblacion = 1000000;
+
+            //long
+            //descripcion: entero 64 bits
+            // long distanciaEstelar = 897484459688L;
+
+            //float
+            //descripcion: numero de punto flotante con precision simple de 32 bits
+            //float temperatura = 36.6f;
+
+            //double
+            //descripcion: numero de punto flotante de precision double de 64 bits
+            // double precio = 19.99;
+
+            //caracter
+            //descripcion= caracter unicode de 16 bits
+            // char iniciar = 'A';
+
+            //booleano
+            //descripcion: valor logico de verdadero o falso
+            //boolean esMayorEdad = true;
+
+    }
+
+
+
+}
