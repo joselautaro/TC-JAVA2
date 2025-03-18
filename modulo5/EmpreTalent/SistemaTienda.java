3972edcf662a2d1096c466ec57ee73d72e380236
Lautaro Montaña <joselautarom@gmail.com>
2025-03-18 08:33:53 -0300

    Sistema Tienda - P2

--- modulo5/EmpreTalent/SistemaTienda.java
+++ modulo5/EmpreTalent/SistemaTienda.java
@@ -1,7 +1,10 @@
 package modulo5.EmpreTalent;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 import javax.swing.JOptionPane;
 
@@ -72,5 +75,63 @@ public class SistemaTienda {
         }
         inventario.add(nuevoProducto);
         System.out.println("Producto agregado satisfactoriamente: " + nuevoProducto);
+    }
+
+    @OperacionSegura(descripcion = "Mostrar inventario")
+    public void mostrarInventario(){
+        if(inventario.isEmpty()){
+            System.out.println("No hay productos en el inventario");
+        }else{
+            StringBuilder sb = new StringBuilder();
+            for(Producto p : inventario){
+                sb.append(p.toString()).append("\n");
+            }
+            JOptionPane.showMessageDialog(null, sb.toString());
+        }
+    }
+
+    @OperacionSegura(descripcion = "Aplicar descuento a todos")
+    public void aplicarDescuentoATodos(){
+        double porcentaje = solicitarDouble("Ingrese el porcentaje de descuento: ");
+
+        inventario.forEach(p -> {
+            p.aplicarDescuento(porcentaje);
+            System.out.println("Descuento aplicado al producto: " + p + " con un " + porcentaje + "% de descuento");
+        });
+    }
+
+    public static void main(String[] args) {
+        SistemaTienda sistema = new SistemaTienda();
+
+        Map<Integer, Consumer<Void>> menuOpciones = new HashMap<>();
+
+        menuOpciones.put(1, (v) -> sistema.agregarProducto());
+        menuOpciones.put(2, (v) -> sistema.mostrarInventario());
+        menuOpciones.put(3, (v) -> sistema.aplicarDescuentoATodos());
+
+        boolean salir = false;
+
+        while (!salir) {
+            String opcion = sistema.solicitarInput(
+                "1. Agregar producto\n"+
+                "2. Mostrar inventario\n"+
+                "3. Aplicar descuento a todos\n"+
+                "4. Salir\n"+
+                "Seleccione una opción: ");
+                try{
+                    int opcionInt = Integer.parseInt(opcion);
+                    Consumer<Void> operacion = menuOpciones.get(opcionInt);
+                    if(operacion != null){
+                        operacion.accept(null);
+                    }else if(opcionInt == 4){
+                        salir = true;
+                        System.out.println("Saliendo del sistema tienda...");
+                    }else{
+                        System.out.println("Opción no válida");
+                    }
+                }catch(NumberFormatException e){
+                    System.out.println("Error, ingrese un número válido");
+                }
+        }
     }
 }
